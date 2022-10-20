2fd50324977de5cf713409eaeba1bd543c29d899
yadhap Dahal <yadhap.dahal@lexisnexisrisk.com>
2022-10-20 16:13:42 -0400

    removed unsued file health.js

--- src/routes/health.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-// Reponds status 200 - OK as long as server is running
-router.get("/health", (req, res) => {
-  res.status(200).json({ status: "OK" });
-});
-
-module.exports = router;
